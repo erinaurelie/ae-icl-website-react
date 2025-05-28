bf1226d8408040ca351feb66088df1a36dc8da17
Erin Aurélie <erinaurelieessama@gmail.com>
2025-05-28 20:33:19 +0000

    feat: added Footer component

--- client/src/sections/Footer.jsx
+++ client/src/sections/Footer.jsx
@@ -1,9 +1,17 @@
 const Footer = () => {
   return (
-    <footer id="contact" aria-label="footer">
-      {/* TODO: Add footer content (links, copyright, etc.) */}
+    <footer aria-label="footer" className="mt-5 text-[#ffffffb3] text-[14px] flex justify-between items-center py-4 px-8 border-t border-[#ffffffb3]">
+      <p>&copy; 2025 AE-International Consulting &amp; Logistics LTD. All rights reserved.</p>
+      <div>
+          <a href="terms-of-service.html" className="mr-10 cursor-pointer hover:underline decoration-white
+">Terms of Service</a>
+          <a href="privacy-policy.html" className="cursor-pointer hover:underline decoration-white
+">Privacy Policy</a>
+      </div>
     </footer>
   )
 }
 
-export default Footer
+export default Footer;
+
+// Todo add Terms of Service and Privacy Policy Page
